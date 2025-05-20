94ec91a1e356652a442efbacc64657816ba8311f
ahmadtahirkamboh <ahmadtahir4235@gmail.com>
2025-05-20 02:03:46 -0700

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "my-terraform-state-bucket"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Dev"
+  }
+}
